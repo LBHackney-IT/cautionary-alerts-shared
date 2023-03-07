28c24970ea242590d1683255ca2b14a3b762fa1d
humulla <43747248+humulla@users.noreply.github.com>
2023-03-07 12:58:04 +0000

    end cautionary alert request

---
+++ Hackney.Shared.CautionaryAlerts/Boundary/Request/EndCautionaryAlert.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hackney.Shared.CautionaryAlerts.Boundary.Request
+{
+    public class EndCautionaryAlert
+    {
+        public string AssureReference { get; set; }
+        public DateTime IncidentDate { get; set; }
+        public string IncidentDescription { get; set; }
+        public Alert Alert { get; set; }
+        public AssetDetails AssetDetails { get; set; }
+        public PersonDetails PersonDetails { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public Guid AlertId { get; set; }
+    }
+}
