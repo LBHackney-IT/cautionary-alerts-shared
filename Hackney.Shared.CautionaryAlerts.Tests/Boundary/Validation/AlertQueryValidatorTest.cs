bf651b2e12312c34984864e2f74b121b6e2d43c6
humulla <43747248+humulla@users.noreply.github.com>
2023-03-08 15:41:37 +0000

    AlertQueryObject with validator and tests

---
+++ Hackney.Shared.CautionaryAlerts.Tests/Boundary/Validation/AlertQueryValidatorTest.cs
@@ -0,0 +1,58 @@
+﻿using FluentAssertions;
+using FluentValidation.TestHelper;
+using Hackney.Shared.CautionaryAlerts.Boundary.Request;
+using Hackney.Shared.CautionaryAlerts.Boundary.Request.Validation;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hackney.Shared.CautionaryAlerts.Tests.Boundary.Validation
+{
+    [TestFixture]
+    public class AlertQueryValidatorTest
+    {
+        private readonly AlertQueryValidator _classUnderTest;
+
+        public AlertQueryValidatorTest()
+        {
+            _classUnderTest = new AlertQueryValidator();
+        }
+
+        [Test]
+        public void AlertIdFailsIfEmpty()
+        {
+            var model = new AlertQueryObject { AlertId = Guid.Empty };
+            var result = _classUnderTest.TestValidate(model);
+            result.ShouldHaveValidationErrorFor(x => x.AlertId);
+
+        }
+
+        [Test]
+        public void AlertIdNotFailsIfValid()
+        {
+            var model = new AlertQueryObject { AlertId = Guid.NewGuid() };
+            var result = _classUnderTest.TestValidate(model);
+            result.ShouldNotHaveValidationErrorFor(x => x.AlertId);
+
+        }
+
+        [Test]
+        public void PersonIdFailsIfEmpty()
+        {
+            var model = new AlertQueryObject { PersonId = Guid.Empty };
+            var result = _classUnderTest.TestValidate(model);
+            result.ShouldHaveValidationErrorFor(x => x.PersonId);
+
+        }
+
+        [Test]
+        public void PersonIdShouldNotFailIfValid()
+        {
+            var model = new AlertQueryObject { PersonId = Guid.NewGuid() };
+            var result = _classUnderTest.TestValidate(model);
+            result.ShouldNotHaveValidationErrorFor(x => x.PersonId);
+
+        }
+    }
+}
